b8e9880171adf073de2a925c64999e5aa8b39ed7
Luizfsl <122307110+Luizfsl@users.noreply.github.com>
2024-05-30 18:06:22 -0300

    Update questao5.c

--- questao5.c
+++ questao5.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+//-> Aqui definimos os valores das variáveis que definiram o tamanho das matrizes
+// -> Foi pensado nos tamanhos que as matrizes que vão ser multiplicar tem que ter para ela ser possível
 #define xA 2
 #define yA 2
 #define x 2
 #define yX 1
+// ---------------------------------------------------------------------------------
 
+// -> Aqui as matrizes sendo declaradas globalmente 
 double A[xA][yA] = {
         {2, 1},
         {5, 7}
@@ -17,12 +21,14 @@ double B[xA][yX] = {
         {11},
         {13}
 };
+// ---------------------------------------------------------------------------------
 
 pthread_barrier_t barrier;
 pthread_barrier_t barrierIntermediária;
 
+// -> Struct que tem a finalidade de armazenar quais dados será realizado por cada thread
 typedef struct Thread_Data{
-    double (*A)[yA]; //-> SIGNIFICADO: A é um ponteiro para uma matriz 2D no qual cada linha tem yA elementos
+    double (*A)[yA]; 
     double (*X)[yX];
     double (*B)[yX];
     double X_Resposta[x][yX];
@@ -31,6 +37,7 @@ typedef struct Thread_Data{
     int startIdx;
     int endIdx;
 }Thread_Data;
+// ---------------------------------------------------------------------------------
 
 void* Jacobi(void *arg);
 
@@ -53,8 +60,8 @@ int main(){
     pthread_t threads[N];
     Thread_Data thread_data[N];
 
-    int segment = qtdInc / N;
-    int resto = qtdInc % N;
+    int segment = qtdInc / N; // Parametro usado para definir a quantidade de incógnicas cada thread irá tratar
+    int resto = qtdInc % N; // Caso a divisão não seja exata, o resto da divisão será armazenado aqui
 
     for (int i = 0; i < N; i++) {
         thread_data[i].A = A;
@@ -63,7 +70,7 @@ int main(){
         thread_data[i].qtdInc = qtdInc;
         thread_data[i].startIdx = i * segment;
         if( i == N - 1){
-            thread_data[i].endIdx = ((i + 1) * segment) + resto - 1;
+            thread_data[i].endIdx = ((i + 1) * segment) + resto - 1; // Para o caso da divisão não seja exata, adicionamos o resto da divisão na última thread
         }
         else{
             thread_data[i].endIdx = ((i + 1) * segment) - 1;
@@ -88,16 +95,15 @@ int main(){
     return 0;
 }
 void* Jacobi(void *arg){
-    printf("ENTROU\n");
     Thread_Data* data = (Thread_Data*)arg;
-    int P = 100; 
+    int P = 100; // Devinimos um parametro de interaçẽs arbritrária, mas que seja alto o suficiente para resolver o problema para poucas incógnitas
 
     for(int k = 0; k < P; k++){
         for(int i = data->startIdx; i <= data->endIdx; i++){
             double sum = 0.0;
             for(int j = 0; j < x; j++){
                 if( i != j){
-                    sum += data->A[i][j] * X[j][0];
+                    sum += data->A[i][j] * X[j][0]; // Essa parte representa o somátorio do algoritmo, e após finalizado, faremos a subtração cujo resultado será dividido
                 }
             }
             X[i][0] = (data->B[i][0] - sum) / data->A[i][i];
