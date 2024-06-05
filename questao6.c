d4dfbab7798cbf47663882a277982dec0d7e3cfe
Nathan Barbosa <nathansantos1713@gmail.com>
2024-06-05 12:07:54 -0300

    Codigo para revisao

--- questao6.c
+++ questao6.c
@@ -1,5 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 //Implementacao de fila para a criacao da lista_pronto
 typedef struct link{
@@ -68,49 +70,62 @@ pthread_t dequeue(Queue *q){
 #define true 1
 #define num_Threads 10 //Quantidade de threads a serem criadas
 Queue *Lista_pronto;
+int emProcesso;
+int threads_executadas;
 pthread_t escalonador; //Thread escalonador
 pthread_t threads[num_Threads]; //Threads a serem criadas
-pthread_mutex_t mutex; //Mutex a ser utilizado em conjunto com as variaveis de condicao
-pthread_cond_t cond; 
+pthread_mutex_t mutex1; //Mutex a ser utilizado em conjunto com as variaveis de condicao
+pthread_mutex_t mutex2; //Mutex para acessar a variavel compartilhada emProcesso
+pthread_mutex_t mutex3;
+pthread_cond_t cond;
 
 void *escalonamento(void *lista_pronto){
     Queue *temp = (Queue *)lista_pronto;
 		while(true){
-      pthread_mutex_lock(&mutex);
+      pthread_mutex_lock(&mutex1);
       while(temp->size == 0){
-        pthread_cond_wait(&cond, &mutex);
+        pthread_cond_wait(&cond, &mutex1);
       }
       
-      while(temp->size > 0){
+      while(temp->size > 0 && emProcesso< N){
         pthread_t Thread_id = dequeue(temp);
-        pthread_mutex_unlock(&mutex);
-        pthread_join(Thread_id, NULL);
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex2);
+        emProcesso++;
+        pthread_mutex_unlock(&mutex2);        
       }
-      pthread_mutex_unlock(&mutex);
+      pthread_mutex_unlock(&mutex1);
       
     }
   return NULL;
 }
 
 void *function(void *arg){
-  
-
-  
+  	Queue *temp = (Queue *)arg;
+  	sleep(3);
+    printf("A thread: %ld esta executando\n", pthread_self());
+  	pthread_mutex_lock(&mutex2);\
+    printf("Threads em processamento: %d\n",emProcesso);
+  	emProcesso--;
+    threads_executadas++;
+  	printf("Threads em processo após decremento: %d\n", emProcesso);
+  	pthread_mutex_unlock(&mutex2);
+  	pthread_exit(NULL);
 }
 void agendar(int index){
     //Cria a thread[index] e a adiciona a lista de threads prontas
-  	pthread_create(&threads[index], NULL, function, NULL);
-      
-    pthread_mutex_lock(&mutex);
+  	pthread_create(&threads[index], NULL, function, (void *)Lista_pronto);  
+    pthread_mutex_lock(&mutex1);
   	enqueue(Lista_pronto, threads[index]); //Adiciona a thread[index] a lista de threads prontas
   	pthread_cond_signal(&cond); //Manda o sinal para o escalonador dizendo que tem item na lista pronto
-  	pthread_mutex_unlock(&mutex);
+  	pthread_mutex_unlock(&mutex1);
 }
 int main(void){
     //Inicializa tanto mutex quanto a variavel de condicao.
-    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&mutex1, NULL);
+  	pthread_mutex_init(&mutex2, NULL);
     pthread_cond_init(&cond, NULL);
+  	emProcesso = 0;
+    threads_executadas = 0;
 
     //Cria a lista pronto
     Lista_pronto = create_queue();
@@ -122,5 +137,11 @@ int main(void){
   	for(int i = 0; i < num_Threads; i++){
     		agendar(i);
     }
-    return 0;
+
+    for(int i = 0; i < num_Threads; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Threads que foram executadas ao todo: %d\n", threads_executadas);
+    pthread_exit(NULL);
 }
